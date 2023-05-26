897284223ba8873436854c680dfbedab4ff04301
juyonLee00 <ljy012900@gmail.com>
2023-05-27 02:28:41 +0900

    #5 자동 파괴 기능 추가 & 속도 지정

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody bulletRigidbody;
+    public float speed = 8f;
+
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
