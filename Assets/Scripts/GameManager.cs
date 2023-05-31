0e431b3fe1f0c736e5b3c5e77672a64b54a88bfa
juyonLee00 <ljy012900@gmail.com>
2023-05-31 19:09:54 +0900

    #11 생존 시간 표시

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    //생존 시간 갱신
+    private float surviveTime;
+    public Text surviveTimeText;
+    private bool isGameOver;
+
+    void Start()
+    {
+        surviveTime = 0f;
+        isGameOver = false;
+    }
+
+    void Update()
+    {
+        if(!isGameOver)
+        {
+            surviveTime += Time.deltaTime;
+            surviveTimeText.text = "Time : " + (int)surviveTime;
+        }
+    }
+}
